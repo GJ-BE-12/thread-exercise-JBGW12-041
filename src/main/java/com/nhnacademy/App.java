df6d436ea44957cef685f7ea50c42e7c9a8fcebb
R4mel <dev_20@naver.com>
2025-08-20 16:38:03 +0900

    한인혁 circular-wait

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -16,16 +16,14 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class App {
-
     private static final Object resource1 = new Object();
     private static final Object resource2 = new Object();
 
     public static void main(String[] args) {
-
         //TODO#1 Thread-1 가 resource2의 접근 권한을 획득한 상태에서 resource1의 접근 권한을 대기하고 있습니다.
         Thread thread1 = new Thread(() -> {
             synchronized (resource2) {
-                log.debug("{}: locked resource 2", Thread.currentThread().getName() );
+                log.debug("{}: locked resource 2", Thread.currentThread().getName());
 
                 try {
                     Thread.sleep(1000);
@@ -43,7 +41,7 @@ public class App {
         //TODO#2 Thread-2 가 resource1의 접근 권한을 획득한 상태에서 resource2의 접근 권한을 대기하고 있습니다.
         Thread thread2 = new Thread(() -> {
             synchronized (resource1) { // resource1을 먼저 잠금
-                log.debug("{}: locked resource 1",Thread.currentThread().getName());
+                log.debug("{}: locked resource 1", Thread.currentThread().getName());
 
                 try {
                     Thread.sleep(1000);
@@ -52,7 +50,7 @@ public class App {
                 }
 
                 synchronized (resource2) {
-                    log.debug("{}: locked resource 2",Thread.currentThread().getName());
+                    log.debug("{}: locked resource 2", Thread.currentThread().getName());
                 }
             }
         });
